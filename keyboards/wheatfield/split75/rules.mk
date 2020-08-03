558551ba56597a2a9892e98f175bfffee0559d29
Oldrich Svec <oldrich.svec@gmail.com>
2020-08-03 13:09:03 +0200

    adding my keyboard

--- keyboards/wheatfield/split75/rules.mk
+++ keyboards/wheatfield/split75/rules.mk
@@ -15,12 +15,12 @@ BOOTLOADER = bootloadHID
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 WS2812_DRIVER = i2c
 
 # custom matrix setup
